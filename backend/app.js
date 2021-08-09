22c6bdfede5cc795c8bb62035ca9ea7df8cbbbcf
weijiangyang <escape>
2021-08-09 11:14:41 +0200

    update

---
+++ backend/app.js
@@ -0,0 +1,49 @@
+
+const express = require('express');
+const stuffRoutes = require('./routes/stuff')
+const app = express();
+ const userRoutes = require('./routes/user');
+ const path = require('path');
+
+
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+mongoose.connect('mongodb+srv://weijiangyang:Ywj32559438@cluster0.bjkt6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+app.use(bodyParser.json());
+
+app.use('/api/stuff',stuffRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+
+app.use('/api/auth', userRoutes);
+var session = require("express-session");
+var NedbStore = require('nedb-session-store')( session );
+const sessionMiddleware = session({
+    secret: "fas fas",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      path: '/',
+      httpOnly: true,
+      maxAge: 365 * 24 * 60 * 60 * 1000   // e.g. 1 year
+    },
+    store: new NedbStore({
+      filename: 'path_to_nedb_persistence_file.db'
+    })
+  });
+
+  app.use(sessionMiddleware);
+  
+
+
+module.exports = app;
---
+++ backend/controllers/stuff.js
@@ -0,0 +1,42 @@
+const Thing = require('../models/thing');
+
+
+exports.createThing = (req, res, next) => {
+  const thingObject = JSON.parse(req.body.thing);
+  delete thingObject._id;
+  const chose = new Thing({
+    ...thingObject,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  });
+  chose.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.getAllStuff = (req, res, next) => {
+Thing.find()
+.then(things => res.status(200).json(things))
+.catch(error => res.status(400).json({ error }));
+};
+
+exports.getOneThing = (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+};
+
+exports.updateThing = (req, res, next) => {
+  const thingObject = req.file ?
+    {
+      ...JSON.parse(req.body.thing),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+  Thing.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+};
+exports.deleteThing = (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+};
