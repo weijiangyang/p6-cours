3ea041df6e80e9cde9555ef29e1b62b2a9dfe6a2
weijiangyang <escape>
2021-08-12 21:52:39 +0200

    --- Auto-Git Commit ---

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -15,10 +15,31 @@ exports.signup = (req,res) => {
     })
     .catch(err => res.status(400).json({err}));
     
-
-
 };
 
+
+
 exports.login = (req,res) => {
+    User.findOne({email:req.body.email})
+    .then(user => {
+        if(!user){
+            return res.status(401).json({message:'utilisateur non trouve!'})
+        }
+        bcrypt.compare(req.body.password,user.password)
+        .then(valid =>{
+            if(!valid){
+                return res.status(401).json({message:"mot de pass incorrect!"});
+            }
+            res.status(200).json({
+                userId: user._id,
+                token:'TOKEN'
+            })
+        }
+            
+        )
+        .catch(err)
+    })
+    .catch(err)
+
 
 }
